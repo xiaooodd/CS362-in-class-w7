c00619d83ee6fff64e9780b54ebec92e589420bf
xiaooodd <63233120+xiaooodd@users.noreply.github.com>
2021-05-16 14:16:15 +0800

    error change

--- p_pytest.py
+++ p_pytest.py
@@ -1,14 +1,14 @@
 import pytest
 import palindrome
 
-def test1(self):
+def test1():
     result = palindrome.is_p("a")
     assert result == True
 
-def test2(self):
+def test2():
     result = palindrome.is_p("a++a")
     assert result == True
 
-def test3(self):
+def test3():
     result = palindrome.is_p("a456sd")
     assert result == False
--- w_pytest.py
+++ w_pytest.py
@@ -1,14 +1,14 @@
 import pytest
 import wordcount
 
-def test1(self):
+def test1():
     result = wordcount.w_count(" a ")
     assert result == 1
 
-def test2(self):
+def test2():
     result = wordcount.w_count("Hello world!")
     assert result == 2
 
-def test3(self):
+def test3():
     result = wordcount.w_count("a++++ b c")
     assert result == 3
